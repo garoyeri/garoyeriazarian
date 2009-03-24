1bcb9204db987cc74330b988eea766c87d6f42b7
garo.yeriazarian <garo.yeriazarian@f4a933f2-c654-0410-b806-cb9529aa5dc2>
2009-03-24 06:24:22 +0000

    initial commit.

---
+++ ExtensibleDataBinding/trunk/DataBinding.Tests/MapFacts.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+using DataBinding.Operations;
+
+namespace DataBinding.Tests
+{
+	public class MapFacts
+	{
+		[Fact]
+		public void can_map_simple_values()
+		{
+			var source = new InjectedSource<string>();
+			var sink = new InjectedSink<string>();
+			source.Value = "One";
+			var map = new Map<string>(source, sink);
+
+			source.Value = "Two";
+
+			Assert.Equal("Two", sink.Value);
+		}
+	}
+
+	class InjectedSource<T> : Source<T>
+	{
+		T _value;
+
+		public T Value
+		{
+			get
+			{
+				return _value;
+			}
+			set
+			{
+				if (value.Equals(this.Value)) return;
+
+				_value = value;
+
+				var changed = this.ValueChanged;
+				if (changed == null) return;
+
+				changed(this, EventArgs.Empty);
+			}
+		}
+
+		public event EventHandler ValueChanged;
+	}
+
+	class InjectedSink<T> : Sink<T>
+	{
+		public T Value
+		{
+			get;
+			set;
+		}
+	}
+}
---
+++ ExtensibleDataBinding/trunk/DataBinding.Tests/TestTypes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace DataBinding.Tests
+{
+	class SampleBase : INotifyPropertyChanged
+	{
+		protected void FirePropertyChanged(string propname)
+		{
+			var propchanged = this.PropertyChanged;
+			if (propchanged == null) return;
+
+			propchanged(this, new PropertyChangedEventArgs(propname));
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+
+
+	class Sample1 : SampleBase
+	{
+		string _name, _description;
+
+		public string Name
+		{
+			get { return _name; }
+			set
+			{
+				if (this.Name == value) return;
+
+				_name = value;
+				FirePropertyChanged("Name");
+			}
+		}
+
+		public string Description
+		{
+			get { return _description; }
+			set
+			{
+				if (this.Description == value) return;
+
+				_description = value;
+				FirePropertyChanged("Description");
+			}
+		}
+	}
+}
